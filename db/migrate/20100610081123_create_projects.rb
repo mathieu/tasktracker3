5a81ac9d01ce0f93abe199f96de94fff3947eab9
Mathieu Marache <mathieu@marache.com>
2010-06-10 10:11:55 +0200

    Added project scaffolded

---
+++ db/migrate/20100610081123_create_projects.rb
@@ -0,0 +1,15 @@
+class CreateProjects < ActiveRecord::Migration
+  def self.up
+    create_table :projects do |t|
+      t.string :id
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :projects
+  end
+end
