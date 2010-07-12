5a86b04ff6d05d9850c17073c9bd97e9368272ac
Mathieu Marache <mathieu@marache.com>
2010-07-12 17:37:22 +0200

    added unit tests

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,15 @@
 class Activity < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+  validates_presence_of :description
   validates_presence_of :project
-  validates_presence_of :user 
+  validates_presence_of :user
+  validate :one_date_for_user
+
+protected
+  def one_date_for_user
+    unless user.nil?
+      errors.add(:date, 'you have already an activity recorded for this date') if user.activities.include?(:date)
+    end
+  end
 end
--- test/unit/activity_test.rb
+++ test/unit/activity_test.rb
@@ -5,4 +5,16 @@ class ActivityTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "test invalid empty Activity" do
+    activity = Activity.new
+    assert !activity.valid?
+    assert !activity.user.valid?
+    assert !activity.project.valid?
+  end
+
+  test "one activity per date for one user" do
+    activity = Activity.create(:date => '20100711', :user => User.all[0], :project => Project.all[0])
+    assert !activity.valid?
+  end
 end
