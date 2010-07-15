5662a9495e8451e79e360e4f1007969fe6e13758
mathieu <mathieu@marache.com>
2010-07-15 23:01:05 +0200

    added config for ActionMailer in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ TaskTracker3::Application.configure do
   # In production, Apache or nginx will already do this    
   # needed for Heroku
   config.serve_static_assets = true
-
+                                                                     <
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
@@ -44,4 +44,7 @@ TaskTracker3::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+
+  # devise needs this for
+  config.action_mailer.default_url_options = { :host => 'http://tasktracker3.heroku.com' }
 end
