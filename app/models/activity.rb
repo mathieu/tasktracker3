8bea51372065b0fcbffa6c03809d648e623c317d
Guillaume Besse <gui.besse@gmail.com>
2011-02-11 18:52:22 +0100

    add day_percent column to activities table

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,6 @@ class Activity < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :project
   validates_presence_of :user
+  validates_numericality_of :day_percent, :only_integer => true, :less_than_or_equal_to => 100
+  
 end
